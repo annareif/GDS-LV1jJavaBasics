f71dfd58a7cdde521c7b2d259c447ff4b3d87a2c
Anna <s56645@edu.campus02.at>
2025-09-29 19:27:05 +0200

    Kommentare hinzugefügt

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 8;
+        int ergebnis = a + b;
+        String name = "MyCalculator";
+
+        System.out.println("Rechner");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Ergebnis = " + ergebnis);
+        System.out.println("Ergebnis = " + ergebnis); //STRG + D -> duplizieren
+
+
+
+    }
+}

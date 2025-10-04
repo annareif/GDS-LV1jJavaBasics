e19094f8c219c6c025910dc020d6997405db4e40
Anna <s56645@edu.campus02.at>
2025-10-04 08:28:20 +0200

    Kommentare hinzugefügt

--- src/CalculatorMethoden.java
+++ src/CalculatorMethoden.java
@@ -2,38 +2,32 @@ public class CalculatorMethoden {
 
     public static void main(String[] args) {
         int a = 4;
-        int b = 3;
+        int b = 2;
+        String methodName = "Addition";
 
-        int result = add(a, b);
-        System.out.println("Input 1 = " + a);
-        System.out.println("Input 2 = " + b);
-        System.out.println("Result = " + result );
+        int result = add(a,b);
+        System.out.println("Methode" + methodName);
+        System.out.println("Wert 1: " + a);
+        System.out.println("Wert 2: " + b);
+        System.out.println("Resultat: " + result);
 
 
     }
 
-    public static void printResult(int a, int b, int result) {
-        System.out.println("Input 1 = " + a);
-        System.out.println("Input 2 = " + b);
-        System.out.println("Result = " + result );
+    public static void printresult(int a, int b, int result, String methodName) {
+
     }
-
-
     public static int add(int a, int b) {
         return a + b;
     }
-
-    public static int substract(int a, int b) {
+    public static int sub(int a, int b) {
         return a - b;
     }
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+    public static double div(int a, int b) {
+        return a / (b * 1.0);
+    }
 
-    public static double divide (int a, int b) {
-        if (b != 0) {
-            return a / (b * 1.0);
-        } else {
-            return 0;
-        }
-
-
-    }}
-
+}

662764c024f91d2437056ff225f7b1e627a613bb
Anna <s56645@edu.campus02.at>
2025-10-05 17:51:43 +0200

    Kommentare hinzugefügt

--- src/Tagesabschnitt.java
+++ src/Tagesabschnitt.java
@@ -1,12 +1,18 @@
 public class Tagesabschnitt {
     public static void main(String[] args) {
 
-        int uhrzeit = 9;
+        int uhrzeit = 6;
 
-        if (uhrzeit < 9); {
-        System.out.println("Früh");
-        }
-
+        if (uhrzeit >5 && uhrzeit <= 9) {
+            System.out.println("Früh");
+        } else if (uhrzeit > 9 && uhrzeit <= 12) {
+            System.out.println("Vormittag");
+        } else if (uhrzeit > 12 && uhrzeit <= 16) {
+            System.out.println("Nachmittag");
+        } else if (uhrzeit> 16 && uhrzeit <= 20) {
+            System.out.println("Abend");
+        }else
+            System.out.println("Nacht");
 
     }
 }
